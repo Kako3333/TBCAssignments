2c73c8ae84d74c5f4a004d82dd4025938c77bfca
Giorgi Kalaladze <124840662+Kako3333@users.noreply.github.com>
2024-12-09 15:41:35 +0400

    Assignment29

---
+++ Assignment29/Assignment29/Assignment29App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment29App.swift
+//  Assignment29
+//
+//  Created by Gio Kakaladze on 08.12.24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment29App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
